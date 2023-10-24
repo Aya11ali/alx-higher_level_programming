910ee68b4a13bbe67ddaffb75db834ca124aff66
Aya11ali <alyzaghlol340@gmail.com>
2023-10-24 20:30:23 +0300

    ..

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""square  class"""
+
+
+class Square:
+    """square  """
+
+
+
+    def __init__(self, size):
+        """init
+        Args:
+            size: size of this square
+        """
+
+        self._size = size
