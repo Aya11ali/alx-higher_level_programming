52914196672f4b010fd99a14ebe71658858fe9aa
Aya11ali <alyzaghlol340@gmail.com>
2023-10-24 21:27:10 +0300

    ..

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """init
+        Args:
+            size: size of square
+        """
+        self.size = size
+
+        @property
+        def size(self):
+            """size getter"""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """size setter"""
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            elif not isinstance(size, int):
+                raise TypeError("size must be an integer")
+
+        self.__size = value
+
+    def area(self):
+        """area of a square"""
+        return self.__size * self.__size
