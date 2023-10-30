4b4984988448d21272954dda6572ce9166007454
Aya11ali <alyzaghlol340@gmail.com>
2023-10-30 12:09:31 +0200

    ..

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""2-Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """init
+        Args:
+            width: int
+            height: int
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter method
+           checking width value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter method
+           checking height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """return the perimeter of rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """returns a rectangle with the character #"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        rec = (self.width * "#" + "\n") * (self.height -1) + (self.width * "#")
+        return rec
+
+    def __repr__(self):
+        """return rectangle arguments"""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """print delete messageeee"""
+        print("Bye rectangle...")
+
